9581b070b44b0dadc0840655165ecefae26e4b88
thuyencode <thuyencode@proton.me>
2023-11-04 22:21:52 +0700

    CSS Styling, Optimizing Fonts and Images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import '@/app/ui/global.css'
 import { ReactNode } from 'react'
+import { inter } from './ui/fonts'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={`antialiased ${inter.className}`}>{children}</body>
     </html>
   )
 }
