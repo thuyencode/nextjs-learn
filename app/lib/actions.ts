ab75caf8a3d669be8e2668cd446b53672f7b9893
thuyencode <thuyencode@proton.me>
2023-11-06 22:39:13 +0700

    Mutating Data

---
+++ app/lib/actions.ts
@@ -0,0 +1,61 @@
+'use server'
+
+import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+
+const InvoicesURL = '/dashboard/invoices'
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true })
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0]
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `
+
+  revalidatePath(InvoicesURL)
+  redirect(InvoicesURL)
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+
+  const amountInCents = amount * 100
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+
+  revalidatePath(InvoicesURL)
+  redirect(InvoicesURL)
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath(InvoicesURL)
+}
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { createInvoice } from '@/app/lib/actions'
 import { CustomerField } from '@/app/lib/definitions'
 import { Button } from '@/app/ui/button'
 import {
@@ -12,7 +13,7 @@ import Link from 'next/link'
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className='rounded-md bg-gray-50 p-4 md:p-6'>
         {/* Customer Name */}
         <div className='mb-4'>
@@ -73,6 +74,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type='radio'
                   value='pending'
                   className='h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600'
+                  defaultChecked
                 />
                 <label
                   htmlFor='pending'
